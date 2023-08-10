65b45485d1b88196e4f9f0494ae1b0e32337d2d5
Ayman91 <ayman.mosstafa@gmail.com>
2023-08-10 14:56:44 -0700

    Task: UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,17 @@
 #include <unistd.h>
 
-/*
+/**
  * main - Entry point
  *
- *Return: Always 1 (Success)
- * */
+ * Description:
+ * The main function, a quote is written to error output using write.
+ * message: "and that piece of art is useful\" - Dora Korpar, 2015-10-19".
+ * The program exits with a status of 1.
+ *
+ * Return: Always 1 (Success)
+ */
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
