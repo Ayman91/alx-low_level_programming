1a90b2eb854772329c4973bd5be147999193f15e
Ayman91 <ayman.mosstafa@gmail.com>
2023-08-10 04:42:59 -0700

    Task: Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - Entry point of the program
+ * main - Entry point
  *
  * Description:
- *     This function is the entry point of the program. It uses the puts function
- *     to print the message "Programming is like building a multilingual puzzle"
- *     to the console. The escaped double quotes are used to include actual quotes
- *     within the output. The program then returns 0 to indicate successful execution.
+ * This function is the entry point of the program. It uses the puts function
+ * to print the message "Programming is like building a multilingual puzzle"
+ * to the console. The escaped double quotes are used to include actual quotes
+ * in output. The program returns 0 to indicate successful execution.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
