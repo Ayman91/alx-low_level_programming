3086629aa7185b58d30e7fac07311880960896c7
Ayman91 <ayman.mosstafa@gmail.com>
2023-08-10 04:24:22 -0700

    Task: Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+* Function main - is the entry point of the program.
+* Function puts - prints a string message included in the double quots.
+* Return -  exists the program and returns 0. means the the code runs successfully
+**/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
