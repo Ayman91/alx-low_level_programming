1f516e00fbc3045843df87fb8f97c0ceea5109cf
Ayman91 <ayman.mosstafa@gmail.com>
2023-08-10 04:53:39 -0700

    Task: Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+/*
+*Description:
+*main - entry point function.
+*printf - is print function.
+*prints message between qoutation marks.
+*ended with new line \n.
+*return 0 for successful execution.
+*/
 
-int main(void){
+int main(void)
+{
 printf("with proper grammer, but the outcome is a piece of art,\n");
 return (0);
 }
