c7b571811bc64c9b9c1eeed393f4b283c8fb4e28
Ayman91 <ayman.mosstafa@gmail.com>
2023-08-10 04:32:32 -0700

    Task: Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+
 /**
+*
 * Desciption:  
 * Function main - is program entry point.
 * Function puts - prints a string messagedouble quots.
+*
 * Return -  exists the program and returns 0.
 **/
 int main(void)
