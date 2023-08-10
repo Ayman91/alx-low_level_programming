55c50e5b609832ee36d8b67ebe5b0babc3e3fb41
Ayman91 <ayman.mosstafa@gmail.com>
2023-08-10 05:00:32 -0700

    Task: Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
-/*
-*Description:
-*main - entry point function.
-*printf - is print function.
-*prints message between qoutation marks.
-*ended with new line \n.
-*return 0 for successful execution.
-*/
 
+/**
+ * main - Entry point
+ *
+ * Description:
+ * This function is the entry point of the program.The printf function
+ * prints "with proper grammar, but the outcome is a piece of art,"
+ * The program returns 0 to indicate successful execution.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-printf("with proper grammer, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,");
 return (0);
 }
