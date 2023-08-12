bf296d4a980b5807e0064d4e3d2c8702111b0e65
Ayman91 <ayman.mosstafa@gmail.com>
2023-08-11 19:41:02 -0700

    Task: Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,28 +12,24 @@
  */
 int main(void)
 {
-	int first_digit = 0, second_digit;
+	int first_digit, second_digit;
 
-	for (; first_digit <= 99;)
+	for (first_digit = 0; first_digit <= 98; first_digit++)
 	{
-		for (; second_digit <= 99;)
+		for (second_digit = first_digit + 1; second_digit <= 99; second_digit++)
 		{
-			if (first_digit != second_digit)
+			putchar((first_digit / 10) + '0');
+			putchar((first_digit % 10) + '0');
+			putchar(' ');
+			putchar((second_digit / 10) + '0');
+			putchar((second_digit % 10) + '0');
+			if (first_digit == 98 && second_digit == 99)
 			{
-				putchar((first_digit / 10) + 48);
-				putchar((first_digit % 10) + 48);
-				putchar(' ');
-				putchar((second_digit / 10) + 48);
-				putchar((second_digit % 10) + 48);
-				if (first_digit != 98 || second_digit != 99)
-				{
-					putchar(',');
-					putchar(' ');
-				}
+				continue;
 			}
-			second_digit++;
+			putchar(',');
+			putchar(' ');
 		}
-		first_digit++;
 	}
 	putchar('\n');
 	return (0);
